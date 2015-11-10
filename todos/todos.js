685430f4b4bd352b8523318f3bc23116c756bf47
imran_nazir <imran.nazir@yahoo.co.uk>
2015-11-10 12:05:11 +0000

    user accounts added and insecure package removed

--- todos/todos.js
+++ todos/todos.js
@@ -1,7 +1,8 @@
+// create a new Mongo db collection
 Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient) {
-    // Template Helpers
+    // Template Helpers, methods that are available to the templates
     Template.main.helpers({
         todos: function() {
             return Todos.find({}, {
@@ -13,15 +14,13 @@ if (Meteor.isClient) {
             });
         }
     });
-
+    // Events attached to the template
     Template.main.events({
         'submit .new-todo': function(event) {
+            // 'text' is the 'name' of the form
             var text = event.target.text.value;
 
-            Todos.insert({
-                text: text,
-                createdAt: new Date()
-            });
+            Meteor.call('addTodo', text);
 
             // clear form
             event.target.text.value = "";
@@ -31,14 +30,24 @@ if (Meteor.isClient) {
         },
 
         'click .toggle-checked': function () {
-          Todos.update(this._id,  {$set:{checked: ! this.checked}});
+          Meteor.call('setChecked', this._id, !this.checked);
         },
 
         'click .delete-todo': function () {
           if (confirm('Are you sure')) {
-            Todos.remove(this._id);
+            Meteor.call('deleteTodo', this._id);
           };
         }
+    });
+
+    Accounts.ui.config({
+      requestPermissions: {
+        // facebook: ['user_likes']
+      },
+      requestOfflineToken: {
+        // google: true
+      },
+      passwordSignupFields: 'USERNAME_ONLY' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
     });
 }
 
@@ -47,3 +56,28 @@ if (Meteor.isServer) {
         // code to run on server at startup
     });
 }
+
+// Meteor.methods needed once insecure package removed
+// to perform db writes
+Meteor.methods({
+  addTodo: function (text) {
+    if (!Meteor.userId) {
+      throw new Meteor.error('not-authorized');
+    };
+
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      UserId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  deleteTodo: function (todoId) {
+    Todos.remove(todoId);
+  },
+
+  setChecked: function (todoId, setChecked) {
+    Todos.update(todoId, {$set:{checked: setChecked}});
+  }
+});
