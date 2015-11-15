e48bd1965c177ab193508072b32f610d0c67e9a8
imran_nazir <imran.nazir@yahoo.co.uk>
2015-11-15 14:06:30 +0000

    adding schema to data, make it more secure

--- microposts/client/common.js
+++ microposts/client/common.js
@@ -2,3 +2,8 @@ accountsUIBootstrap3.logoutCallback = function(error) {
     if (error) console.log("Error:" + error);
     Router.go('posts');
 }
+
+Accounts.ui.config({
+    requestPermissions: {},
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
--- microposts/lib/collections.js
+++ microposts/lib/collections.js
@@ -1,5 +1,7 @@
+// Profile Images Collection
 ProfileImages = new FS.Collection('ProfileImages', {
 	stores: [new FS.Store.GridFS('ProfileImages')]
 });
 
+// Collection that connects the users to the images
 UserImages = new Mongo.Collection('UserImages');
