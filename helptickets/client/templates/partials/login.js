2e646cfc884326584a5079303ad674c8fbab67d0
imran_nazir <imran.nazir@yahoo.co.uk>
2015-12-01 17:32:04 +0000

    created initial folder structure, templates and code for login/register system

---
+++ helptickets/client/templates/partials/login.js
@@ -0,0 +1,87 @@
+Template.login.events({
+    'click .register-link': function(event) {
+        $('.panel-register').fadeIn();
+        $('.panel-login').hide();
+    },
+    'click .login-link': function(event) {
+        $('.panel-register').hide();
+        $('.panel-login').fadeIn();
+    },
+    'submit .register-form': function(event) {
+        var email = trimInput(event.target.email.value);
+        var password = trimInput(event.target.password.value);
+        var password2 = trimInput(event.target.password2.value);
+
+        if (isNotEmpty(email) &&
+            isNotEmpty(password) &&
+            isEmail(email) &&
+            areValidPasswords(password, password2)) {
+            // Create New user
+            Accounts.createUser({
+                password: password,
+                email: email,
+                profile: {
+                    usertype: 'customer'
+                }
+            }, function(err) {
+                if (err) {
+                    FlashMessages.sendError(err.reason)
+                } else {
+                    FlashMessages.sendSuccess('Account Created! You are now logged in');
+                    Router.go('/');
+                };
+            });
+        }
+        return false;
+
+    }
+});
+
+// Validation Rules
+
+// Trim Helper
+var trimInput = function(val) {
+    return val.replace(/^\s*|\s*s/g, '')
+}
+
+// Check For Empty Fields
+isNotEmpty = function(value) {
+    if (value && value !== '') {
+        return true
+    };
+    FlashMessages.sendError('Please fill in all the fields');
+    return false;
+}
+
+// validate email
+isEmail = function(value) {
+    var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+    if (filter.test(value)) {
+        return true
+    };
+    FlashMessages.sendError("Please use a valid email address");
+    return false;
+};
+
+// check password field
+isValidPassword = function(password) {
+    if (password.length < 6) {
+        FlashMessages.sendError("Password must be at least 6 charachters");
+        return false;
+    };
+    return true;
+};
+
+// Match Passwords
+areValidPasswords = function(password, confirm) {
+    if (!isValidPassword(password)) {
+        return false;
+    };
+
+    if (password !== confirm) {
+        FlashMessages.sendError("Passwords Do Not Match");
+        return false;
+    };
+    return true;
+};
