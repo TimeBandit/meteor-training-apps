323eac336d112ff509eb3d47f6aa629f9874a9ca
imran_nazir <imran.nazir@yahoo.co.uk>
2015-12-01 12:38:00 +0000

    completed login/logout functionality, created dashboard layout & template

--- authapp/lib/router.js
+++ authapp/lib/router.js
@@ -9,3 +9,21 @@ Router.route('/', function () {
 Router.route('/register', function () {
 	this.render('register');
 });
+
+// this router uses a custom layout
+// different from the other layouts
+// see https://iron-meteor.github.io/iron-router/#hooks
+// for different way of calling onBeforeAcrion
+Router.route('/dashboard', {
+	layoutTemplate: 'dashboard_layout',
+	path: '/dashboard',
+	tempalte: 'dashboard',
+	onBeforeAction: function(){
+		if (Meteor.user()== null) {
+			FlashMessages.sendError('Please Login :)');
+			Router.go('/');
+		};
+		// continue calling the next function
+		this.next();
+	}
+});
