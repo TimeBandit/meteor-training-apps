88f589bb2de681bd046ca27bf3222763c88ae891
imran_nazir <imran.nazir@yahoo.co.uk>
2015-12-10 12:41:36 +0000

    completed login functionality

--- codefolio/client/templates/helpers.js
+++ codefolio/client/templates/helpers.js
@@ -1,16 +1,36 @@
 Template.login.events({
-	'submit .login-user': function (event) {
-		var email = event.target.email.value;
-		var password = event.target.password.value;
+    'submit .login-user': function(event) {
+        var username = event.target.username.value;
+        var password = event.target.password.value;
+        
 
-		Meteor.loginWithPassword(email, password, function(err){
-			if (err) {
-				event.target.email.value = email;
-				event.target.password.value = password;
+        Meteor.loginWithPassword(username, password, function(err) {
+            if (err) {
+                event.target.username.value = username;
+                event.target.password.value = password;
+                
+                // if this is missspelt you get erroneous errors
+                FlashMessages.sendError(err.reason);
+            } else {
+                FlashMessages.sendSuccess('You are now logged in');
+                Router.go('/admin/projects');
+            };
+        });
 
-				FlashMessage.sendError(err.reason);
-			} else{
-				FlashMessages.sendSuccess('You are now logged in');
+        event.target.username.value = "";
+        event.target.password.value = "";
+        // prevent form submission
+        return false;
+    }
+});
+
+// reference the container template instead of 'login'
+Template.layout.events({
+	'click .logout-user': function () {
+		Meteor.logout(function(err){
+			if (err) {} else{
+				FlashMessages.sendSuccess('You are now logged out');
+				Router.go('/admin');
 			};
 		});
 	}
