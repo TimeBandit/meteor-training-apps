faaf83bf084cc191e5b23d0b2ce75057689fa855
imran_nazir <imran.nazir@yahoo.co.uk>
2015-12-26 19:35:00 +0000

    completing CRUD

--- techmeetups/both/controllers/dashboard.js
+++ techmeetups/both/controllers/dashboard.js
@@ -1,17 +1,48 @@
 DashboardController = AppController.extend({
-  waitOn: function() {
-    return this.subscribe('items');
-  },
-  data: {
-    items: Items.find({})
-  },
-  onAfterAction: function () {
-    Meta.setTitle('Dashboard');
-  }
+    waitOn: function() {
+        return this.subscribe('mymeetups');
+    }
+});
+
+DashboardController.helpers({
+    'mymeetups': function() {
+        return Meetups.find({
+            user: Meteor.userId()
+        });
+    }
 });
 
 DashboardController.events({
-  'click [data-action=doSomething]': function (event, template) {
-    event.preventDefault();
-  }
+    'submit .update-meetup-form': function(event) {
+        var title = event.target.title.value;
+        var email = event.target.email.value;
+        var topics = event.target.topics.value;
+        var type = event.target.type.value;
+        var address = event.target.address.value;
+        var city = event.target.city.value;
+        var state = event.target.state.value;
+        var zipcode = event.target.zipcode.value;
+        var meetupdate = event.target.meetupdate.value;
+
+        var params = {
+            title: title,
+            email: email,
+            topics: topics,
+            type: type,
+            address: address,
+            city: city,
+            state: state,
+            user: Meteor.userId(),
+            username: Meteor.user().username,
+            zipcode: zipcode,
+            meetupdate: meetupdate,
+            createdAt: new Date()
+        };
+
+        // insert meetup
+        Meteor.call('addMeetup', params);
+        toastr.success('Meetup Added');
+        Router.go('/meetups');
+        return false;
+    }
 });
