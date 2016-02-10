6ead2fea07c97322fd56f7d39b22ffee7d8e2b7f
imran_nazir <imran.nazir@yahoo.co.uk>
2016-02-10 23:03:53 +0000

    completed sidebar collapsed menu

--- spatpost/client/components/layout/MainLayout.jsx
+++ spatpost/client/components/layout/MainLayout.jsx
@@ -1,5 +1,14 @@
 MainLayout = React.createClass({
-
+	componentDidMount: function() {
+		// Menu settings
+		$('#menuToggle, .menu-close').on('click', function(){
+			$('#menuToggle').toggleClass('active');
+			$('body').toggleClass('body-push-toleft');
+			$('#theMenu').toggleClass('menu-open');
+			console.log('toggle clicked');
+		});
+	},
+	
 	render: function() {
 		return (
 			<div>
--- spatpost/client/components/sidebar/Sidebar.jsx
+++ spatpost/client/components/sidebar/Sidebar.jsx
@@ -3,18 +3,18 @@ Sidebar = React.createClass({
 	render: function() {
 		return (
 			/* Menu */
-			<nav class="menu" id="theMenu">
-				<div class="menu-wrap">
-					<h1 class="logo"><a href="index.html#home">Minimal</a></h1>
-					<i class="icon-remove menu-close"></i>
-					<a href="#home" class="smoothScroll">Home</a>
-					<a href="#about" class="smoothScroll">About</a>
-					<a href="#portfolio" class="smoothScroll">FAQ</a>
-					<a href="#contact" class="smoothScroll">Contact</a>
+			<nav className="menu" id="theMenu">
+				<div className="menu-wrap">
+					<h1 className="logo"><a href="index.html#home">Spatpost</a></h1>
+					<i className="menu-close"><span className="glyphicon glyphicon-remove"></span></i>
+					<a href="#home" className="smoothScroll"><span className="glyphicon glyphicon-home"></span></a>
+					<a href="#about" className="smoothScroll">About</a>
+					<a href="#portfolio" className="smoothScroll">FAQ</a>
+					<a href="#contact" className="smoothScroll">Contact</a>
 				</div>
 				
 				{/* Menu button */}
-				<div id="menuToggle"><i class="icon-reorder"></i></div>
+				<div id="menuToggle"><i className="glyphicon glyphicon-menu-hamburger"></i></div>
 			</nav>
 		);
 	}
